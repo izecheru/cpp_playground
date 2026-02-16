305c6dfbe66240f790c425907cb666c6af9dae4c
izecheru <zecheru.ionut.roberto@gmail.com>
2026-02-16 10:50:22 +0200

    Vulkan testing
    This is done with glfw, need to switch to sdl

--- enttTest.cpp
+++ enttTest.cpp
@@ -1,335 +1,194 @@
-﻿#define SOL_ALL_SAFETIES_ON 1
-#include <array>
-#include <entt/entt.hpp>
-#include <entt/meta/factory.hpp> // Needed for the .type<T>() factory
-#include <entt/meta/meta.hpp>
-#include <filesystem>
-#include <fstream>
-#include <glm/glm.hpp>
+﻿#include <cstdlib>
+#include <cstring>
+#include <algorithm>
 #include <iostream>
 #include <memory>
-#include <sol/sol.hpp>
-#include <string>
-#include "entity.hpp"
-#include "event_dispatcher.hpp"
-#include "registry.hpp"
-using namespace entt::literals;
+#include <stdexcept>
+#include <vector>
 
-[[nodiscard]] auto getTypeId( const sol::table& obj ) -> entt::id_type
-{
-  // be careful for this one to be written exactly the way you store it
-  const auto f = obj["typeId"].get<sol::function>();
-  assert( f.valid() && "type_id not exposed to lua!" );
-  return f.valid() ? f().get<entt::id_type>() : -1;
-}
+#if defined( __INTELLISENSE__ ) || !defined( USE_CPP20_MODULES )
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
 
-template <typename TEvent>
-[[nodiscard]] auto deduceType( TEvent&& obj ) -> entt::id_type
+#define GLFW_INCLUDE_VULKAN // REQUIRED only for GLFW CreateWindowSurface.
+#include <GLFW/glfw3.h>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+const std::vector<char const*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication
 {
-  switch ( obj.get_type() )
+public:
+  void run()
   {
-  // if we have registry:has(entity, Transform.type_id()) in lua file the it is a number
-  case sol::type::number:
-    return obj.template as<entt::id_type>();
-  // if we have registry:has(entity, Transform) then we have a table, like struct {}
-  case sol::type::table:
-    return getTypeId( obj );
-  }
-  assert( false );
-  return -1;
-}
-
-// https://github.com/skypjack/entt/wiki/Crash-Course:-runtime-reflection-system
-template <typename... Args>
-inline auto invokeMetaFunc( entt::meta_type meta_type, entt::id_type function_id, Args&&... args ) -> entt::meta_any
-{
-  if ( !meta_type )
-  {
-    std::cout << "meta type is not registered in entt::meta";
-  }
-  else
-  {
-    if ( auto&& metaFunction = meta_type.func( function_id ); metaFunction )
-      return metaFunction.invoke( {}, std::forward<Args>( args )... );
-  }
-  return entt::meta_any{};
-}
-
-template <typename... Args>
-inline auto invokeMetaFunc( entt::id_type type_id, entt::id_type function_id, Args&&... args ) -> entt::meta_any
-{
-  return invokeMetaFunc( entt::resolve( type_id ), function_id, std::forward<Args>( args )... );
-}
-
-template <typename TComponent>
-bool hasComponent( Registry& registry, entt::entity entity )
-{
-  return registry.hasComponent<TComponent>( entity );
-}
-
-/**
- * @brief Adds a component to an entity in the regsitry
- * @tparam TComponent Type of component we add
- * @param registry The registry we currently modify
- * @param entity The entity we add the component to
- * @param comp The lua component
- * @param currentState Lua state NEEDED FOR MAKE REFERENCE!!!
- * @return A reference to the created component
- */
-template <typename TComponent>
-auto addComponent( Registry& registry, entt::entity entity, const sol::table& comp, sol::this_state currentState )
-{
-  auto& component =
-    registry.addComponent<TComponent>( entity, comp.valid() ? std::move( comp.as<TComponent&&>() ) : TComponent{} );
-  // this is what we need the current lua state for
-  return sol::make_reference( currentState, std::ref( component ) );
-}
-
-template <typename TComponent>
-auto getComponent( Registry& registry, entt::entity entity, sol::this_state currentState )
-{
-  auto& component = registry.getComponent<TComponent>( entity );
-  return sol::make_reference( currentState, std::ref( component ) );
-}
-
-template <typename TComponent>
-auto emplaceComponent( Registry& registry, entt::entity entity, const sol::table& comp, sol::this_state currentState )
-{
-  auto& component =
-    registry.emplaceComponent<TComponent>( entity, comp.valid() ? std::move( comp.as<TComponent&&>() ) : TComponent{} );
-
-  // this is what we need the current lua state for
-  return sol::make_reference( currentState, std::ref( component ) );
-}
-
-template <typename TComponent>
-auto removeComponent( Registry& registry, entt::entity entity )
-{
-  registry.removeComponent<TComponent>( entity );
-}
-
-template <typename TComponent>
-void registerMetaComponent()
-{
-  entt::meta_factory<TComponent>()
-    .type( entt::type_hash<TComponent>::value() )
-    .template func<&addComponent<TComponent>>( "add"_hs )
-    .template func<&getComponent<TComponent>>( "get"_hs )
-    .template func<&hasComponent<TComponent>>( "has"_hs )
-    .template func<&removeComponent<TComponent>>( "remove"_hs )
-    .template func<&emplaceComponent<TComponent>>( "emplace"_hs );
-}
-
-template <typename Event>
-auto add_handler( EventDispatcher& dispatcher, const sol::function& f )
-{
-  return std::make_unique<LuaEventHandler<Event>>( dispatcher, f );
-}
-
-template <typename Event>
-auto add_handler2( EventDispatcher& dispatcher, const sol::object& func )
-{
-  if ( !func.valid() )
-  {
-    std::cout << "invalid func\n";
-    return;
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
   }
 
-  auto* funcRef{ func.as<LuaHandler<Event>*>() };
-  funcRef->connection = dispatcher.addHandler<Event, &LuaHandler<Event>::handle>( *funcRef );
-}
+private:
+  GLFWwindow* window = nullptr;
 
-template <typename Event>
-void dispatch_event( EventDispatcher& dispatcher, const sol::table& evt )
-{
-  dispatcher.dispatchEvent( evt.as<Event>() );
-}
+  vk::raii::Context context;
+  vk::raii::Instance instance = nullptr;
+  vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
 
-template <typename TEvent>
-bool has_handlers( EventDispatcher& dispatcher )
-{
-  return dispatcher.hasHandlers<TEvent>();
-}
+  void initWindow()
+  {
+    glfwInit();
 
-template <typename TEvent>
-void registerMetaEvent()
-{
-  entt::meta_factory<TEvent>()
-    .type( entt::type_hash<TEvent>::value() )
-    .template func<&add_handler<TEvent>>( "add_handler"_hs )
-    .template func<&add_handler2<TEvent>>( "add_handler2"_hs )
-    .template func<&has_handlers<TEvent>>( "has_handlers"_hs )
-    .template func<&dispatch_event<TEvent>>( "dispatch_event"_hs );
-}
+    glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
+    glfwWindowHint( GLFW_RESIZABLE, GLFW_FALSE );
 
-struct FileComponent
-{
-  std::string filePath;
+    window = glfwCreateWindow( WIDTH, HEIGHT, "kogayonon game engine", nullptr, nullptr );
+  }
+
+  void initVulkan()
+  {
+    createInstance();
+    setupDebugMessenger();
+  }
+
+  void mainLoop()
+  {
+    while ( !glfwWindowShouldClose( window ) )
+    {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup()
+  {
+    glfwDestroyWindow( window );
+
+    glfwTerminate();
+  }
+
+  void createInstance()
+  {
+    vk::ApplicationInfo appInfo{};
+    appInfo.pApplicationName = "kogayonon";
+    appInfo.applicationVersion = VK_MAKE_VERSION( 1, 0, 0 );
+    appInfo.pEngineName = "kogayonon";
+    appInfo.engineVersion = VK_MAKE_VERSION( 1, 0, 0 );
+    appInfo.apiVersion = vk::ApiVersion14;
+
+    // Get the required layers
+    std::vector<char const*> requiredLayers;
+    if ( enableValidationLayers )
+    {
+      requiredLayers.assign( validationLayers.begin(), validationLayers.end() );
+    }
+
+    // Check if the required layers are supported by the Vulkan implementation.
+    auto layerProperties = context.enumerateInstanceLayerProperties();
+    for ( auto const& requiredLayer : requiredLayers )
+    {
+      if ( std::ranges::none_of( layerProperties, [requiredLayer]( auto const& layerProperty ) {
+             return strcmp( layerProperty.layerName, requiredLayer ) == 0;
+           } ) )
+      {
+        throw std::runtime_error( "Required layer not supported: " + std::string( requiredLayer ) );
+      }
+    }
+
+    // Get the required extensions.
+    auto requiredExtensions = getRequiredExtensions();
+
+    // Check if the required extensions are supported by the Vulkan implementation.
+    auto extensionProperties = context.enumerateInstanceExtensionProperties();
+    for ( auto const& requiredExtension : requiredExtensions )
+    {
+      if ( std::ranges::none_of( extensionProperties, [requiredExtension]( auto const& extensionProperty ) {
+             return strcmp( extensionProperty.extensionName, requiredExtension ) == 0;
+           } ) )
+      {
+        throw std::runtime_error( "Required extension not supported: " + std::string( requiredExtension ) );
+      }
+    }
+
+    vk::InstanceCreateInfo createInfo{};
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledLayerCount = static_cast<uint32_t>( requiredLayers.size() );
+    createInfo.ppEnabledLayerNames = requiredLayers.data();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>( requiredExtensions.size() );
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+    instance = vk::raii::Instance( context, createInfo );
+  }
+
+  void setupDebugMessenger()
+
+  {
+    if ( !enableValidationLayers )
+      return;
+
+    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags( vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+                                                         vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                                                         vk::DebugUtilsMessageSeverityFlagBitsEXT::eError );
+    vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags( vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                                                        vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+                                                        vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation );
+    vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT{};
+    debugUtilsMessengerCreateInfoEXT.messageSeverity = severityFlags;
+    debugUtilsMessengerCreateInfoEXT.messageType = messageTypeFlags;
+    debugUtilsMessengerCreateInfoEXT.pfnUserCallback = &debugCallback;
+
+    debugMessenger = instance.createDebugUtilsMessengerEXT( debugUtilsMessengerCreateInfoEXT );
+  }
+
+  std::vector<const char*> getRequiredExtensions()
+
+  {
+    uint32_t glfwExtensionCount = 0;
+    auto glfwExtensions = glfwGetRequiredInstanceExtensions( &glfwExtensionCount );
+
+    std::vector extensions( glfwExtensions, glfwExtensions + glfwExtensionCount );
+    if ( enableValidationLayers )
+    {
+      extensions.push_back( vk::EXTDebugUtilsExtensionName );
+    }
+
+    return extensions;
+  }
+
+  static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback( vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
+                                                         vk::DebugUtilsMessageTypeFlagsEXT type,
+                                                         const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                                                         void* )
+  {
+    if ( severity == vk::DebugUtilsMessageSeverityFlagBitsEXT::eError ||
+         severity == vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning )
+    {
+      std::cerr << "validation layer: type " << to_string( type ) << " msg: " << pCallbackData->pMessage << std::endl;
+    }
+
+    return vk::False;
+  }
 };
 
-struct IndexComponent
+int main()
 {
-  uint32_t index{ 0 };
-};
+  try
+  {
+    HelloTriangleApplication app;
+    app.run();
+  }
+  catch ( const std::exception& e )
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-struct TransformComponent
-{
-  glm::vec3 pos{};
-  glm::vec3 rotation{};
-  glm::vec3 scale{};
-};
-
-auto collectTypes( const sol::variadic_args& va )
-{
-  std::set<entt::id_type> types;
-  std::transform( va.cbegin(), va.cend(), std::inserter( types, types.begin() ), []( const auto& obj ) {
-    return deduceType( obj );
-  } );
-  return types;
-}
-
-template <typename TEvent, typename TData>
-TData getField( entt::meta_any& object, const char* fieldName )
-{
-  auto type = entt::resolve<TEvent>();
-  auto field = type.data( entt::hashed_string::value( fieldName ) );
-
-  if ( !field )
-    throw std::runtime_error( "Field not found" );
-
-  return field.get( object ).cast<TData>();
-}
-
-struct LuaEvent
-{
-  sol::object data;
-};
-
-auto main() -> int
-{
-  sol::state lua;
-  lua.open_libraries( sol::lib::base, sol::lib::package, sol::lib::string );
-
-  registerMetaComponent<FileComponent>();
-  registerMetaComponent<TransformComponent>();
-  registerMetaComponent<IndexComponent>();
-
-  lua.new_usertype<FileComponent>(
-    "FileComponent",
-    "typeId",
-    &entt::type_hash<FileComponent>::value,
-    sol::call_constructor,
-    sol::factories( []( const std::string& path ) { return FileComponent{ .filePath = path }; } ),
-    "filePath",
-    &FileComponent::filePath );
-
-  lua.new_usertype<IndexComponent>(
-    "IndexComponent",
-    "typeId",
-    &entt::type_hash<IndexComponent>::value,
-    sol::call_constructor,
-    sol::factories( []( const uint32_t index ) { return IndexComponent{ .index = index }; } ),
-    "index",
-    &IndexComponent::index );
-
-  lua.new_usertype<ScriptEvent>(
-    "ScriptEvent",
-    sol::call_constructor,
-    sol::factories( []() { return ScriptEvent{}; }, []( sol::table data ) { return ScriptEvent{ .data = data }; } ),
-    "typeId",
-    &entt::type_hash<ScriptEvent>::value,
-    "data",
-    &ScriptEvent::data );
-
-  lua.new_usertype<LuaHandler<LuaEvent>>(
-    "LuaEventHandler",
-    "typeId",
-    entt::type_hash<LuaHandler<LuaEvent>>::value,
-    "eventType",
-    entt::type_hash<LuaEvent>::value,
-    sol::call_constructor,
-    sol::factories( []( const sol::function& func ) { return LuaHandler<LuaEvent>{ .callback = func }; } ),
-    "release",
-    &LuaHandler<LuaEvent>::release );
-
-  lua.new_usertype<LuaEvent>( "LuaEvent",
-                              "typeId",
-                              &entt::type_hash<LuaEvent>::value,
-                              sol::call_constructor,
-                              sol::factories( []( const sol::object& data ) { return LuaEvent{ .data = data }; } ),
-                              "data",
-                              &LuaEvent::data );
-
-  lua.new_usertype<MessageEvent>(
-    "MessageEvent",
-    sol::call_constructor,
-    sol::factories( []() { return MessageEvent{ "empty" }; }, // Default constructor
-                    []( const std::string& message ) { return MessageEvent{ .message = message }; } // Data constructor
-                    ),
-    "typeId",
-    &entt::type_hash<MessageEvent>::value,
-    "message",
-    &MessageEvent::message );
-
-  EventDispatcher dispatcher{};
-  lua.new_usertype<EventDispatcher>(
-    "EventDispatcher",
-    sol::call_constructor,
-    sol::factories( [&]( bool lua ) {
-      // if lua == true then the dispatcher is local to lua
-      if ( lua )
-        return EventDispatcher{};
-
-      // this is a reference
-      return dispatcher;
-    } ),
-    "hasHandlers",
-    []( EventDispatcher& self, const sol::table& eventTypeOrId ) {
-      if ( getTypeId( eventTypeOrId ) == entt::type_hash<ScriptEvent>::value() )
-      {
-        auto any = invokeMetaFunc( deduceType( eventTypeOrId ), "has_handlers"_hs, self );
-        return any ? any.cast<bool>() : false;
-      }
-      return false;
-    },
-    "addHandler",
-    []( EventDispatcher& self, const sol::object& eventTypeOrId, const sol::object& listener, sol::this_state s ) {
-      if ( !listener.valid() )
-      {
-        return entt::meta_any{};
-      }
-      return invokeMetaFunc( getTypeId( eventTypeOrId ), "add_handler"_hs, self, listener );
-    },
-    "addHandlerExperimental",
-    []( EventDispatcher& self, const sol::object& eventTypeOrId, const sol::object& listener, sol::this_state s ) {
-      if ( !listener.valid() )
-      {
-        return entt::meta_any{};
-      }
-      return invokeMetaFunc( getTypeId( eventTypeOrId ), "add_handler2"_hs, self, listener );
-    },
-    "dispatchEvent",
-    []( EventDispatcher& self, const sol::table& event ) {
-      const auto eventId = getTypeId( event );
-
-      if ( eventId == entt::type_hash<ScriptEvent>::value() )
-      {
-        // this should go only to the lua side
-        self.dispatchEvent<ScriptEvent>( ScriptEvent{ event } );
-      }
-      else
-      {
-        // this goes to lua as well as c++
-        invokeMetaFunc( eventId, "dispatch_event"_hs, self, event );
-      }
-    } );
-
-  registerMetaEvent<ScriptEvent>();
-  registerMetaEvent<MessageEvent>();
-  registerMetaEvent<LuaEvent>();
-  registerMetaEvent<LuaHandler<LuaEvent>>();
-
-  lua.safe_script_file( "H:\\Git\\cpp_playground\\test.lua" );
-
-  return 0;
+  return EXIT_SUCCESS;
 }
